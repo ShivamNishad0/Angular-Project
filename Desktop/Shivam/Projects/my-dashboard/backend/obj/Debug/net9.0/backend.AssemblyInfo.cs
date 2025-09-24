c71e284f514f729ffebaa8a5b1a53150a55d06d0
shivamnishad0 <2k23.mca2313672@gmail.com>
2025-09-24 21:25:07 +0530

    home page added

--- Desktop/Shivam/Projects/my-dashboard/backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
+++ Desktop/Shivam/Projects/my-dashboard/backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+891626372d63e6b90d18ee19471f3f3a340ef775")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+471122b11b81e6bcd869a8af23939f0277e74fa7")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
